fa3e78d608a6a7c15dd07e584f9ba97d73b13c01
Mccoy <bwm38@thoth.cs.pitt.edu>
2023-06-18 14:26:12 -0400

    .

--- reveal.c
+++ reveal.c
@@ -21,10 +21,11 @@ void reveal(FILE* file, BMP_Header read_bmp_header, DIB_Header read_dib_header) 
 
             
             // if there's an encoded 0 for the pixel then its the end of the text so break
-            if (((pixel_array.green & 0x0F )== '\0') && ((pixel_array.red & 0x0F )== '\0')) {
+            if ((pixel_array.blue == '\0') && (pixel_array.green == '\0') && (pixel_array.red == '\0')) {
                 flag = 1;
                 break;
             } 
+
 
             // transforming it
             char g_lsb = pixel_array.green & 0x0F; // 4 LSB of green preceded by 0000
